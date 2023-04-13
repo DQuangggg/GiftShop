1fde8472cb3b4784aa5098a5c860893cac7d5dad
DQuangggg <quangndhe150537@fpt.edu.vn>
2023-04-13 08:11:55 +0700

    create Account Controller

---
+++ Assignment/src/java/Controller/Account/addAccountController.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package Controller.Account;
+
+import DAO.DAOAccount;
+import Entity.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class addAccountController extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.sendRedirect("addAccount.jsp");;
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+       String user = request.getParameter("username");
+        String pass = request.getParameter("password");
+        String admin = request.getParameter("isAdmin");
+        boolean isadmin = Boolean.parseBoolean(admin);
+
+        DAOAccount ac = new DAOAccount();
+        //Check Account exist
+        Account a = ac.checkAccountsExist(user);
+        if (a != null) {
+            request.setAttribute("alertMess", "Username has been existed.");
+            request.getRequestDispatcher("addAccount.jsp").forward(request, response);
+        } else {
+            Account acc = new Account();
+            acc.setUser(user);
+            acc.setPass(pass);
+            acc.setIsAdmin(isadmin);
+            
+            //addAccount
+            ac.addAccountByAdmin(acc);
+            response.sendRedirect("managerAccount");
+        }
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
