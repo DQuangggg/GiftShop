f93f4f4da82792c7fafb12384be106fc33b66fdc
DQuangggg <quangndhe150537@fpt.edu.vn>
2023-04-18 07:51:29 +0700

    add new Controller

---
+++ Assignment/src/java/Controller/Product/managerController.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package Controller.Product;
+
+import DAO.DAOCategory;
+import DAO.DAOProduct;
+import Entity.Category;
+import Entity.Product;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class managerController extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet managerController</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet managerController at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+       ArrayList<Product> products = new ArrayList<>();
+        DAOProduct pd = new DAOProduct();
+        int totalPage = 0;
+        int pagesize = 15;
+        totalPage = pd.getTotalPage(pagesize);
+        String pageCurrent = request.getParameter("page");
+        int pageC = 0;
+        if (pageCurrent == null) {
+            pageC = 1;
+        } else {
+            pageC = Integer.parseInt(pageCurrent);
+
+        }
+        products = pd.getProductWithPaging(pageC, pagesize);
+        ArrayList<Category> listCategory = new ArrayList<>();
+        DAOCategory cd = new DAOCategory();
+        listCategory = cd.getCategory();
+        request.setAttribute("listC", listCategory);
+        request.setAttribute("totalpage", totalPage);
+        request.setAttribute("pageCurrent", pageC);
+        request.setAttribute("listP", products);
+        request.getRequestDispatcher("manager.jsp").forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String pidString = request.getParameter("pid");
+        int pid = Integer.parseInt(pidString);
+        DAOProduct pd = new DAOProduct();
+        Product p = new Product();
+        p = pd.getProductById(pid);
+        ArrayList<Category> listCategory = new ArrayList<>();
+        DAOCategory cd = new DAOCategory();
+        listCategory = cd.getCategory();
+        //if found product
+        if (p != null) {
+            ArrayList<Product> products = new ArrayList<>();
+            products.add(p);
+            request.setAttribute("searchMessage", pid);
+            request.setAttribute("listP", products);
+            request.setAttribute("listC", listCategory);
+            request.getRequestDispatcher("manager.jsp").forward(request, response);
+        } else {
+            request.setAttribute("searchMessage", "No products found");
+            request.setAttribute("listC", listCategory);
+            request.getRequestDispatcher("manager.jsp").forward(request, response);
+        }
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Assignment/src/java/Controller/Product/searchProductController.java
@@ -0,0 +1,156 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller.Product;
+
+import DAO.DAOCategory;
+import DAO.DAOProduct;
+import Entity.Category;
+import Entity.Product;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class searchProductController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet searchProductController</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet searchProductController at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String nameSearch = request.getParameter("q");
+        DAOProduct pd = new DAOProduct();
+
+        int totalPage = 0;
+        int pagesize = 12;
+        String pageCurrent = request.getParameter("page");
+        int pageC = 0;
+        if (pageCurrent == null) {
+            pageC = 1;
+        } else {
+            pageC = Integer.parseInt(pageCurrent);
+        }
+        totalPage = pd.getTotalPageSreachByName(nameSearch, pagesize);
+        ArrayList<Product> products = new ArrayList<>();
+        products = pd.getProductSearchByNameWithPaging(nameSearch, pageC, pagesize);
+        if (products.isEmpty() == false) {
+            boolean issearch = true;
+            ArrayList<Category> listCategory = new ArrayList<>();
+            DAOCategory cd = new DAOCategory();
+            listCategory = cd.getCategory();
+            request.setAttribute("listC", listCategory);
+            request.setAttribute("listP", products);
+
+            request.setAttribute("issearch", issearch);
+            request.setAttribute("textsearch", nameSearch);
+            request.setAttribute("totalpage", totalPage);
+            request.setAttribute("pageCurrent", pageC);
+
+            request.getRequestDispatcher("shop.jsp").forward(request, response);
+        } else {
+            ArrayList<Category> listCategory = new ArrayList<>();
+            DAOCategory cd = new DAOCategory();
+            listCategory = cd.getCategory();
+            boolean issearch = true;
+            request.setAttribute("issearch", issearch);
+
+            request.setAttribute("listC", listCategory);
+            request.setAttribute("textsearch", "Don't find any product have name contain " + nameSearch);
+            request.getRequestDispatcher("shop.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String nameSearch = request.getParameter("q");
+        DAOProduct pd = new DAOProduct();
+        int totalPage = 0;
+        int pagesize = 12;
+        String pageCurrent = request.getParameter("page");
+        int pageC = 0;
+        if (pageCurrent == null) {
+            pageC = 1;
+        } else {
+            pageC = Integer.parseInt(pageCurrent);
+
+        }
+        totalPage = pd.getTotalPageSreachByName(nameSearch, pagesize);
+        ArrayList<Product> products = new ArrayList<>();
+        products = pd.getProductSearchByNameWithPaging(nameSearch, pageC, pagesize);
+        boolean issearch = true;
+        ArrayList<Category> listCategory = new ArrayList<>();
+        DAOCategory cd = new DAOCategory();
+        listCategory = cd.getCategory();
+        request.setAttribute("listC", listCategory);
+        request.setAttribute("listP", products);
+
+        request.setAttribute("issearch", issearch);
+        request.setAttribute("textsearch", nameSearch);
+        request.setAttribute("totalpage", totalPage);
+        request.setAttribute("pageCurrent", pageC);
+
+        request.getRequestDispatcher("shop.jsp").forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
